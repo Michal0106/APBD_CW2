3695dd2209c881a6c1bbfae53141f6720071c80a
Michal0106 <mmroz656@gmail.com>
2024-03-11 15:46:47 +0100

    Added getAverage method

--- CW_2/ConsoleApp1/ConsoleApp1/Program.cs
+++ CW_2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,3 +2,14 @@
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+static int getAverage(int[] nums)
+{
+    var sum = 0;
+    foreach (var num in nums)
+    {
+        sum += num;
+    }
+
+    return sum / nums.Length;
+}
