f30468f23acb57e3f8e80f0ca2dab501e9bdd0ea
Michal0106 <mmroz656@gmail.com>
2024-03-12 22:00:37 +0100

    Changed variable name to sum_konflikt1

--- CW_2/ConsoleApp1/ConsoleApp1/Program.cs
+++ CW_2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,13 +5,13 @@ Console.WriteLine("Modyfikacja 3");
 
 static int getAverage(int[] nums)
 {
-    var sum = 0;
+    var sum_konflikt1 = 0;
     foreach (var num in nums)
     {
-        sum += num;
+        sum_konflikt1 += num;
     }
 
-    return sum / nums.Length;
+    return sum_konflikt1 / nums.Length;
 }
 int[] nums = {1,2,3,4,5};
 var average = getAverage(nums);
